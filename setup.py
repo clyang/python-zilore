6f8f28a44c0fe9c3245f82b00077ed2404c51cf1
Cheng-Lin Yang <cl.yang@cycarrier.com>
2019-09-21 01:15:50 +0800

    Bump version in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='python-zilore',
-    version='0.0.3',
+    version='0.0.4',
     description="Zilore DNS API Wrapper",
     long_description=long_description,
     long_description_content_type="text/markdown",
